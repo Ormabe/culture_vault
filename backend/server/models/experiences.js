7d4138f62c765bcf821cc758480cba2cb0c54b48
Niko Bella Lewis <bellalewis99@gmail.com>
2017-02-01 18:30:50 -0500

    experience

--- backend/server/models/experiences.js
+++ backend/server/models/experiences.js
@@ -1,12 +1,28 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Experiences = sequelize.define('Experiences', {
-    name: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     fav: DataTypes.INTEGER,
-    quote: DataTypes.STRING,
-    story: DataTypes.BLOB,
-    image: DataTypes.STRING
-  }, {
+    quote: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        max: 300
+      }
+    },
+    story: {
+      type: DataTypes.BLOB,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+     {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
