4042754ecc760df215509f2f8e72124b00cb0c80
ChaniceStl <chanicestlouis2020@gmail.com>
2017-02-02 12:42:34 -0500

    ingredient model

--- backend/server/seeds/index.js
+++ backend/server/seeds/index.js
@@ -1,30 +1,37 @@
 //Import Seed Files
 let users = require('./users-seed.js');
 let locations = require('./locations-seed.js');
+let ingredient = require('./ingredient-seed.js')
 //let experiences = require('/experiences-seed.js');
 
 //Import Models
-let Users = require('../models').Users;
-let Locations = require('../models').Locations;
-//let Experiences = require('../models').Experiences;
+let models = require('../models');
+
 
 const seedFunction = () => {
-	Users.create(users[0])
-	Users.create(users[1])
-	Users.create(users[2])
-	Users.create(users[3])
-	Users.create(users[4])
+	models.Users.create(users[0])
+	models.Users.create(users[1])
+	models.Users.create(users[2])
+	models.Users.create(users[3])
+	models.Users.create(users[4])
 
-	Locations.create(locations[0])
-	Locations.create(locations[1])
-	Locations.create(locations[2])
-	Locations.create(locations[3])
-	Locations.create(locations[4])
-	Locations.create(locations[5])
+	models.Locations.create(locations[0])
+	models.Locations.create(locations[1])
+	models.Locations.create(locations[2])
+	models.Locations.create(locations[3])
+	models.Locations.create(locations[4])
+	models.Locations.create(locations[5])
 
-	//Experiences.create(experiences[0])
-	//Experiences.create(experiences[1])	
-	//Experiences.create(experiences[2])
+	// models.Experiences.create(experiences[0])
+	// models.Experiences.create(experiences[1])	
+	// models.Experiences.create(experiences[2])
+
+	models.Ingredient.create(ingredients[0])
+	models.Ingredient.create(ingredients[1])
+	models.Ingredient.create(ingredients[2])
+	models.Ingredient.create(ingredients[3])
+	models.Ingredient.create(ingredients[4])
+
 }
 
 module.exports = seedFunction
