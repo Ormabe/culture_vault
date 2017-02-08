a69d7cf6c858ec385a1cad35c4dde0081b3e8c1b
Rabiya Watson-Maiorana <ormabe@nyc.rr.com>
2017-02-08 18:27:27 -0500

    Including local changes in main repo

--- frontend/components/main/signupform.js
+++ frontend/components/main/signupform.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import $ from 'jquery';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createUserForm } from '../../actions/signup-action';
+import { bindActionCreators } from 'redux';
+
+import axios from 'axios';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -10,37 +14,60 @@ const muiTheme = getMuiTheme();
 
 const styles = require('./signup-style').formStyles
 
-const SignUpForm = React.createClass({
-  getInitialState() {
-    return ({username:"",email:"",password:""})
-  },
+explore default class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username:"",
+      email:"",
+      password:""
+    }
+  }
   handleKeyPress(e) {
 		if (e.key === 'Enter') {
 			this.handleChange()
 		}
-	},
+	}
 	handleChange (e) {
     const target = e.target
     const value = target.value
     const name = target.name
 
-    this.setState({ [name]: value })
-	},
+    this.setState({ [name]: value }) // Local react state
+	}
   enterUser(e) {
     e.preventDefault()
     const info = this.state
-    const that = this
+    // const that = this
 
-    $.ajax({
-      url: 'api/explore/users',
-      type: 'POST',
-      data: info,
-      success: function(data) {
-        console.log('posted',data)
-        that.setState({username:"",email:"",password:""})
+    axios.post('api/explore/users') // State for the store
+      let user = {
+        username: null,
+        email: null,
+        password: null
       }
-    })
-  },
+        .then(() => {
+          user.username: this.state.username,
+          user.email: this.state.email,
+          user.password: this.state.password
+        })
+        .then(data => {
+          store.dispatch(createUserForm(user))
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    // $.ajax({
+    //   url: 'api/explore/users',
+    //   type: 'POST',
+    //   data: info,
+    //   success: function(data) {
+    //     console.log('posted',data)
+    //     that.setState({username:"",email:"",password:""})
+    //   }
+    // })
+  }
   render () {
     return (
     <MuiThemeProvider muiTheme={muiTheme}>
@@ -82,6 +109,14 @@ const SignUpForm = React.createClass({
     </MuiThemeProvider>
     )
   }
-});
+};
 
-export default SignUpForm
+function mapStateToProps(state) {
+  return {
+    username: state.username,
+    email: state.email,
+    password: state.password
+  }
+}
+
+export default connect(mapStateToProps, { createUserForm })(SignUpForm)
--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux';
 //import reducers file
+<<<<<<< Updated upstream
 import feature from './feature-reducer.js'
 import explore from './explore-reducer.js'
 
@@ -7,6 +8,14 @@ const allReducers = combineReducers({
 	feature,
 	explore
 
+=======
+import featureReducer from './feature-reducer.js'
+import signupReducer from './signup-reducer.js'
+
+const allReducers = combineReducers({
+	featureReducer,
+	signupReducer
+>>>>>>> Stashed changes
 })
 
 export default allReducers;
