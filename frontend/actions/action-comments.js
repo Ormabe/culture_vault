930ecae5e1df20fc1e9070c15966c4c8a8106cf3
Victor Amos <victoramosjr@gmail.com>
2017-02-12 18:42:23 -0500

    Complete Comment Component

--- frontend/actions/action-comments.js
+++ frontend/actions/action-comments.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const FETCH_COMMENTS = 'FETCH_COMMENTS';
-export const CREATE_COMMENT = 'CREATE_COMMENT';
 
 const ROOT_URL = 'http://localhost:2222/api/comments/'
 
@@ -10,21 +9,6 @@ export function fetchComments(id) {
 
 	return {
 		type: FETCH_COMMENTS,
-		payload: request
-	}
-}
-
-export function createComment(comment, experience) {
-	const request = axios.post(`${ROOT_URL}experience/${experience}`, { comment })
-		.then(response => {
-			console.log(response);
-		})
-		.catch(error => {
-			console.log(error);
-		})
-
-	return {
-		type: CREATE_COMMENT,
 		payload: request
 	}
 }
--- frontend/reducers/reducer-comments.js
+++ frontend/reducers/reducer-comments.js
@@ -1,13 +1,11 @@
-import { FETCH_COMMENTS, CREATE_COMMENT } from '../actions/action-comments';
+import { FETCH_COMMENTS } from '../actions/action-comments';
 
-const INITIAL_STATE = { comments: null, currentComment: [] };
+const INITIAL_STATE = { comments: null };
 
 export default function(state = INITIAL_STATE, action) {
 	switch(action.type) {
 		case FETCH_COMMENTS:
 			return { comments: action.payload.data }
-		case CREATE_COMMENT:
-			return Object.assign({}, state, {currentComment:[...state.currentComment, action.payload.data]})
 		default:
 			return state;
 	}
