f898a59842df8bb21a0587ec9603243a5de6e2b0
ChaniceStl <chanicestlouis2020@gmail.com>
2017-02-12 15:56:16 -0500

    Update comment-model.js

--- backend/server/models/comment-model.js
+++ backend/server/models/comment-model.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Comment = sequelize.define('Comment', {
-    comment: DataTypes.STRING,
+    comment: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
