8ea87b413c422a1c9c537862b2d7f1ff716c89c6
ChaniceStl <chanicestlouis2020@gmail.com>
2017-02-16 23:27:59 -0500

    Location Component Complete

--- frontend/actions/location-action.js
+++ frontend/actions/location-action.js
@@ -6,8 +6,7 @@ const ROOT_URL = 'http://localhost:2222/api/explore/country/';
 
 
 export function getCountryExperience(countryId) {
- const request = axios.get(`ROOT_URL${countryId}`);
- 
+ const request = axios.get(`${ROOT_URL}${countryId}`)
 	return {
 		type: 'FETCH_LOCATION',
 		payload: request
